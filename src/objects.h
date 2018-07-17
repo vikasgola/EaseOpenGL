905c05acb045b13d5a9cc4feb51b318f42b23c98
Vikas Gola <vikasgola2015@gmail.com>
2018-07-17 16:17:01 +0530

    objects colors

--- src/objects.h
+++ src/objects.h
@@ -16,14 +16,14 @@ namespace easeopengl{
             GLfloat *vertices;
             GLuint number_of_vertices;
             GLint *indices;
-            glm::vec3 color;
+            glm::vec3 color ;
             VAO<GLfloat, GLint> *vao;
             glm::mat4 model;
             glm::vec3 position = glm::vec3(0.0f,0.0f,0.0f);
             glm::vec3 rotation_axis = glm::vec3(0.0f,0.0f,1.0f);
             GLfloat angle = 0.0f;
             
-            EaseObject(GLuint point_length , GLfloat _vertices[], GLuint number_of_vertices, GLint _indices[] = nullptr,  GLuint number_of_indices = 0, glm::vec3 _color = glm::vec3(1.0f,1.0f,1.0f)){
+            EaseObject(GLuint point_length , GLfloat _vertices[], GLuint number_of_vertices, GLint _indices[] = nullptr,  GLuint number_of_indices = 0, glm::vec3 _color = glm::vec3(0.0f,0.0f,0.0f)){
                 this->vertices = _vertices;
                 this->indices = _indices;
                 this->color = _color;
@@ -51,11 +51,16 @@ namespace easeopengl{
             void draw(GLint draw_using,GLuint shader_program){
                 if(this->indices == nullptr){
                     glUniformMatrix4fv(glGetUniformLocation(shader_program, "model"), 1, GL_FALSE , value_ptr(this->model));
+                    glUniform3f(glGetUniformLocation(shader_program, "object_color"), this->color.r,this->color.g,this->color.b);
                     glBindVertexArray(this->vao->getVAO());
                     glDrawArrays(draw_using , 0 , this->number_of_vertices/this->vao->getVAOdataLength());
                     glBindVertexArray(0);
                 }
 
+            }
+
+            void setColor(GLfloat r,GLfloat g, GLfloat b ){
+                this->color = glm::vec3(r,g,b);
             }
 
             void setPosition(glm::vec3 translate_position){
@@ -67,6 +72,10 @@ namespace easeopengl{
                 this->angle = rotate_angle;
                 this->rotation_axis = rotation_axis;
                 this->model = glm::rotate(this->model , this->angle , this->rotation_axis);
+            }
+
+            void setScale(GLfloat x,GLfloat y, GLfloat z){
+                this->model = glm::scale(this->model,glm::vec3(x,y,z));
             }
 
             void clearModel(){
