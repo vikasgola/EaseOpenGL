572c863928ca6ffb30e852cac749f765166788b9
Vikas Gola <vikasgola2015@gmail.com>
2019-02-03 05:46:16 +0530

    added scripts

---
+++ scripts/requirements.sh
@@ -0,0 +1,3 @@
+sudo apt update
+sudo apt install -y freeglut3-dev mesa-common-dev libglm-dev build-essential libsoil-dev libglfw3-dev libglfw3 libglew-dev
+echo "Done!"
---
+++ scripts/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+g++ -std=c++11 $1 -lGL -lGLEW -lglfw -lSOIL
+
